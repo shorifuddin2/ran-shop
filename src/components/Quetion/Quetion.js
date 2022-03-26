aaf40de5807cf9d5ed7e4a129d9be912d83bf791
shorifuddin2 <shorifuddin240915@gmail.com>
2022-03-26 20:04:02 +0600

    product iems add

--- src/components/Quetion/Quetion.js
+++ src/components/Quetion/Quetion.js
@@ -13,7 +13,9 @@ const Quetion = () => {
         
          
             <h4 className='Queition'>Props Vs State..?</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolorem atque ab aliquid, laboriosam reiciendis iure eius non culpa sit</p>
+            <p>Props are read-only. It allows data to be passed from one element to another. Like an argument passed to a function
+            The state is an updateable structure that is used to hold data or information about the material and may change over time.
+            Determines how it will render.</p>
 
             <h4 className='Queition'>How useState..?</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolorem atque ab aliquid, laboriosam reiciendis iure eius non culpa sit</p>
