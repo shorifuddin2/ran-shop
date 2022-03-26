15e222b25215e3db4966a6b2eb08dbda24f8cba4
shorifuddin2 <shorifuddin240915@gmail.com>
2022-03-26 23:19:17 +0600

    alyes solp

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
+import { ImCross } from 'react-icons/im'
+import Modal from 'react-modal';
 import './Shop.css';
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+  
+  Modal.setAppElement('#root');
 
 const Shop = () => {
     const  [products, setProducts] = useState([])
     const [cart, setCart] =useState([]);
+    const [rendom, setRendom]=useState([])
 
     useEffect(()=>{
         fetch('https://raw.githubusercontent.com/mir-hussain/guns/main/data.json')
@@ -25,6 +40,20 @@ const Shop = () => {
         setCart(newCart);
     }
     
+    const random=(cart)=>{
+        const randomIndex = Math.floor(Math.random()*cart.length)
+        setRendom(cart[randomIndex])
+    }
+
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    //Modal open function
+    function openModal() {
+        setIsOpen(true);
+    }
+    // Modal close function
+    function closeModal() {
+        setIsOpen(false);
+    }
 
     return (
         <div className='shop-container'>
@@ -43,8 +72,18 @@ const Shop = () => {
                     <p>Selected Items: {cart.length}</p>
                     {
                         cart.map(p=> <p>{p.name}</p>)
+                        
                     }
-                    <button className="btn">Select Items</button>
+                    <Modal
+                                        isOpen={modalIsOpen}
+                                        onRequestClose={closeModal}
+                                        style={customStyles}
+                                        contentLabel="Example Modal">
+                                        <ImCross className='cross' onClick={closeModal}></ImCross>
+                                        <h1>{random.name}</h1>
+                                        {/* <img  src={drawItem.img} alt="" /> */}
+                                        </Modal>
+                    <button className="btn" onClick={()=>random(cart)}>Select Items</button>
                     <button className="btn" onClick={()=>remove()}>Remove</button>
                     
                 </div>
--- src/components/Quetion/Quetion.js
+++ src/components/Quetion/Quetion.js
@@ -5,7 +5,7 @@ const Quetion = () => {
     return (
         <div className='Queition-aria'>
             <h4 className='Queition'>How React Works..?</h4>
-            <p>React JS. It is an interactive user interface (UI) based JavaScript framework
+            <p>Answer: React JS. It is an interactive user interface (UI) based JavaScript framework
             A more component rich library
             Very fast with react
             Using for front-end and back-end web development
@@ -13,12 +13,12 @@ const Quetion = () => {
         
          
             <h4 className='Queition'>Props Vs State..?</h4>
-            <p>Props are read-only. It allows data to be passed from one element to another. Like an argument passed to a function
+            <p>Answer: Props are read-only. It allows data to be passed from one element to another. Like an argument passed to a function
             The state is an updateable structure that is used to hold data or information about the material and may change over time.
             Determines how it will render.</p>
 
             <h4 className='Queition'>How useState..?</h4>
-            <p>
+            <p>Answer: 
             useState provides a pair of useState Hook Feedback Hold this status in the render Hook is a function that lets you "hook in" function They allow you to use feedback without class. Hook is a JavaScript function, but they impose two additional rules From the function element</p>
         </div>
     );
